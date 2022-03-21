6cb86e8a31f17e2d83f43573825fecdee71eb85f
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-03-21 11:42:16 +0100

    changes icon, adds logo image

---
+++ src/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>prova</h1>
+      <img src="../assets/logo.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+name : "Logo"
+}
+</script>
+
+<style>
+
+</style>
